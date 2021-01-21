4bb3ad61b53c67f117deed291353a3700dd34179
Christopher W Schultz <chris.william.schultz@gmail.com>
2021-01-20 18:05:55 -0700

    basic tests working

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,39 @@
 module.exports = {
-  success,
-  fail,
-  repair,
-  get,
+    success,
+    fail,
+    repair,
+    get,
 };
 
 function success(item) {
-  return { ...item };
+    return { ...item,
+        enhancement: item.enhancement >= 20 ? item.enhancement : item.enhancement + 1
+    };
 }
 
 function fail(item) {
-  return { ...item };
+    let failedItem = item;
+
+    if(item.enhancement < 15) {
+        failedItem = { ...failedItem, durability: failedItem.durability - 5 }
+    } else if(item.enhancement >= 15) {
+        failedItem = { ...failedItem, durability: failedItem.durability - 10}
+
+        if(item.enhancement > 16) {
+            failedItem = { ...failedItem, enhancement: failedItem.enhancement - 1 }
+        }
+    }
+    return failedItem;
 }
 
 function repair(item) {
-  return { ...item };
+    return { ...item, durability: 100 };
 }
 
 function get(item) {
-  return { ...item };
+    if(item.enhancement > 0) {
+        return { ...item, name: `[+${item.enhancement}] ${item.name}` }
+    } else {
+        return { ...item };
+    }
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,91 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+describe('enhancer', () => {
+    it('repairs', () => {
+        expect(enhancer.repair({
+            name: "item",
+            enhancement: 0,
+            durability: 10,
+        })).toEqual({
+            name: "item",
+            enhancement: 0,
+            durability: 100,
+        });
+    });
+
+    it('success', () => {
+        expect(enhancer.success({
+            name: "item",
+            enhancement: 0,
+            durability: 100,
+        })).toEqual({
+            name: "item",
+            enhancement: 1,
+            durability: 100,
+        });
+
+        expect(enhancer.success({
+            name: "item",
+            enhancement: 20,
+            durability: 80,
+        })).toEqual({
+            name: "item",
+            enhancement: 20,
+            durability: 80,
+        });
+    });
+
+    it('fail', () => {
+        expect(enhancer.fail({
+            name: "item",
+            enhancement: 0,
+            durability: 100,
+        })).toEqual({
+            name: "item",
+            enhancement: 0,
+            durability: 95,
+        });
+
+        expect(enhancer.fail({
+            name: "item",
+            enhancement: 16,
+            durability: 100,
+        })).toEqual({
+            name: "item",
+            enhancement: 16,
+            durability: 90,
+        });
+
+        expect(enhancer.fail({
+            name: "item",
+            enhancement: 20,
+            durability: 100,
+        })).toEqual({
+            name: "item",
+            enhancement: 19,
+            durability: 90,
+        });
+    });
+
+    it('get', () => {
+        expect(enhancer.get({
+            name: 'thing',
+            enhancement: 0,
+            durability: 5
+        })).toEqual({
+            name: 'thing',
+            enhancement: 0,
+            durability: 5
+        });
+
+        expect(enhancer.get({
+            name: 'thing',
+            enhancement: 20,
+            durability: 100
+        })).toEqual({
+            name: '[+20] thing',
+            enhancement: 20,
+            durability: 100
+        });
+    });
+});
